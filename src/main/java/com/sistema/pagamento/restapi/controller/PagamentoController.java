3a0cc084a4b734f59311839be20aa426b4f71bb4
João Paulo Oliveira <joaooliveirawebdeveloper@gmail.com>
2024-04-23 18:32:56 -0300

    fix: remocao das validacoes da classe PagamentoController e inclusao na classe PagamentoController

--- src/main/java/com/sistema/pagamento/restapi/controller/PagamentoController.java
+++ src/main/java/com/sistema/pagamento/restapi/controller/PagamentoController.java
@@ -2,9 +2,7 @@ package com.sistema.pagamento.restapi.controller;
 
 import com.sistema.pagamento.restapi.model.Pagamento;
 import com.sistema.pagamento.restapi.service.PagamentoService;
-import com.sistema.pagamento.restapi.validator.PagamentoValidator;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,26 +12,15 @@ import java.util.List;
 @RequestMapping("/pagamentos")
 public class PagamentoController {
     private final PagamentoService pagamentoService;
-    private final PagamentoValidator pagamentoValidator;
 
     @Autowired
-    public PagamentoController(PagamentoService pagamentoService, PagamentoValidator pagamentoValidator) {
+    public PagamentoController(PagamentoService pagamentoService) {
         this.pagamentoService = pagamentoService;
-        this.pagamentoValidator = pagamentoValidator;
     }
 
     @PostMapping("/receber")
     public ResponseEntity<?> receberPagamento(@RequestBody Pagamento pagamento) {
-        if (!pagamentoValidator.isMetodoPagamentoValido(pagamento.getMetodoPagamento())) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Método de pagamento inválido.");
-        }
-
-        if (pagamentoValidator.isNumeroCartaoRequired(pagamento.getMetodoPagamento()) && pagamento.getNumeroCartao() == null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Número do cartão é obrigatório para pagamento com cartão.");
-        }
-
-        Pagamento novoPagamento = pagamentoService.receberPagamento(pagamento);
-        return ResponseEntity.status(HttpStatus.CREATED).body(novoPagamento);
+        return pagamentoService.receberPagamento(pagamento);
     }
 
     @PutMapping("/{id}/atualizar-status")
--- src/main/java/com/sistema/pagamento/restapi/service/PagamentoService.java
+++ src/main/java/com/sistema/pagamento/restapi/service/PagamentoService.java
@@ -2,7 +2,10 @@ package com.sistema.pagamento.restapi.service;
 
 import com.sistema.pagamento.restapi.model.Pagamento;
 import com.sistema.pagamento.restapi.repository.PagamentoRepository;
+import com.sistema.pagamento.restapi.validator.PagamentoValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,15 +13,28 @@ import java.util.List;
 @Service
 public class PagamentoService {
     private final PagamentoRepository pagamentoRepository;
+    private static final String STATUS_PENDENTE = "Pendente de Processamento";
+
+    private final PagamentoValidator pagamentoValidator;
 
     @Autowired
-    public PagamentoService(PagamentoRepository pagamentoRepository) {
+    public PagamentoService(PagamentoRepository pagamentoRepository, PagamentoValidator pagamentoValidator) {
         this.pagamentoRepository = pagamentoRepository;
+        this.pagamentoValidator = pagamentoValidator;
     }
 
-    public Pagamento receberPagamento(Pagamento pagamento) {
-        pagamento.setStatus("Pendente de Processamento");
-        return pagamentoRepository.save(pagamento);
+    public ResponseEntity<?> receberPagamento(Pagamento pagamento) {
+        if (!pagamentoValidator.isMetodoPagamentoValido(pagamento.getMetodoPagamento())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Método de pagamento inválido.");
+        }
+
+        if (pagamentoValidator.isNumeroCartaoRequired(pagamento.getMetodoPagamento()) && pagamento.getNumeroCartao() == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Número do cartão é obrigatório para pagamento com cartão.");
+        }
+
+        pagamento.setStatus(STATUS_PENDENTE);
+        Pagamento novoPagamento = pagamentoRepository.save(pagamento);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novoPagamento);
     }
 
     public Pagamento atualizarStatusPagamento(Long id, String novoStatus) {
