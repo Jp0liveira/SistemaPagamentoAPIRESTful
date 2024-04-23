d39e9a66cbc81556a6fb8ed09879558986ab315a
João Paulo Oliveira <joaooliveirawebdeveloper@gmail.com>
2024-04-23 16:23:32 -0300

    feat: Inclusao do Repository

---
+++ src/main/java/com/sistema/pagamento/restapi/repository/PagamentoRepository.java
@@ -0,0 +1,10 @@
+package com.sistema.pagamento.restapi.repository;
+
+import com.sistema.pagamento.restapi.model.Pagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Long> {
+
+}
