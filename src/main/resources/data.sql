abc674f0134ca1f20b7540488f73914f403bbb1c
João Paulo Oliveira <joaooliveirawebdeveloper@gmail.com>
2024-04-23 20:58:07 -0300

    feat: inclusao de novos parametros no data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,8 @@
 -- Inserir valores pré-definidos na tabela Pagamento
 INSERT INTO Pagamento (codigo_debito, cpf_cnpj_pagador, metodo_pagamento, numero_cartao, valor, status)
 VALUES (1, '12345678900', 'boleto', null, 100.00, 'Pendente de Processamento'),
-       (2, '98765432100', 'cartao_credito', '1234 5678 9012 3456', 150.00, 'Pendente de Processamento'),
-       (3, '98765432100', 'pix', null, 200.00, 'Pendente de Processamento');
+       (2, '98765432100', 'cartao_credito', '1234 5678 9012 3456', 150.00, 'Processado com Sucesso'),
+       (3, '98765432100', 'cartao_debito', '1234 5678 9012 3456', 540.00, 'Pendente de Processamento'),
+       (4, '98765432100', 'boleto', null, 0, 'Processado com Falha'),
+       (5, '98765432100', 'pix', null, 0, 'Inativo');
+
